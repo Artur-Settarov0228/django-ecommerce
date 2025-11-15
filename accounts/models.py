ac71ce2991f4e62f7e373c6b87cce8fb81740600
Artur-Settarov0228 <settarovartur2@gmail.com>
2025-11-15 22:39:52 +0500

    accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=200, unique=True)
+    password = models.CharField(max_length=128)
+    email = models.EmailField(unique=True, null=True, blank=True)
+    phone = models.CharField(max_length=15, unique=True, null=True, blank=True)
+
+
+    def __str__(self):
+        return self.username
+    
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    last_name = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200)
+    bio = models.CharField(max_length=300, blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', default='avatars/default.png') 
+
+    def __str__(self):
+        return self.user.username
+
+
+    
+    
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+import json
+from django.views import View
 
-# Create your views here.
+from django.http import HttpRequest , JsonResponse
+from . models import User, Profile
+
+
+class AccountView(View):
+
+    def post(self, request: HttpRequest) -> JsonResponse:
+
+        data = json.loads(request.body.decode())
+
+        try:
+            User.objects.get(username = data['username'])
+
+            return JsonResponse({'massage': 'Username yaratib bo`lgan'}, status=400)
+        except User.DoesNotExist:
+            user = User(
+                username = data['username'],
+                password = data['password'],
+                email = data.get('email'),
+                phone = data.get('phone')
+
+            )
+            user.save()
+
+            profile = Profile(user = user)
+            profile.save()
+
+            return JsonResponse(data={'massage': 'user yaratildi'}, status = 201)
+
+
+        
+       
+    
